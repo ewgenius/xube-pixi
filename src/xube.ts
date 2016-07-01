fe84fdf9d76fa54fe355746a4aac8d536a468ed1
ewgenius <ewgeniux@gmail.com>
2016-07-01 10:06:28 +0300

    modules

--- src/xube.ts
+++ src/xube.ts
@@ -1,27 +1,2 @@
-import * as PIXI from 'pixi.js'
-import {
-  Container,
-  autoDetectRenderer
-} from 'pixi.js'
-
-class Game {
-  private renderer = autoDetectRenderer(800, 600, { backgroundColor: 0x000000 })
-  private stage = new Container()
-
-  run() {
-    document.body.appendChild(this.renderer.view)
-    
-    this.loop()
-  }
-
-  private loop() {
-    requestAnimationFrame(() => this.loop())
-  }
-
-  static main() {
-    const game = new Game()
-    game.run()
-  }
-}
-
+import {Game} from './engine/Game'
 Game.main()
