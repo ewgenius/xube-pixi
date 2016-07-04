5dbb2715419d70b2cd355f49e1049fba8f91cccd
ewgenius <ewgeniux@gmail.com>
2016-07-04 14:57:12 +0300

    ecs

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -3,33 +3,83 @@ import {
   autoDetectRenderer,
   Container,
   Texture,
-  Sprite
+  DisplayObject,
+  Sprite,
+  EventEmitter,
+  Point
 } from 'pixi.js'
+import {
+  System,
+  Component,
+  Entity,
+  World
+} from './ecs'
 
 const texture1: string = require<string>('../../assets/texture1.jpg')
 
+class TransformComponent extends Component {
+  position: Point = new Point(0, 0)
+}
+
+class SpriteComponent extends Component {
+  sprite: Sprite
+
+  constructor(sprite: Sprite) {
+    super()
+    this.sprite = sprite
+  }
+}
+
+class RenderingSystem extends System {
+  private renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer
+  private root: Container
+
+  constructor(root: Container, renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer) {
+    super()
+    this.renderer = renderer
+    this.root = root
+  }
+
+  update() {
+    this.renderer.render(this.root)
+  }
+}
 
 export class Game {
   private renderer = autoDetectRenderer(800, 600, { backgroundColor: 0x000000 })
   private stage = new Container()
+  private world = new World()
 
   run() {
     document.body.appendChild(this.renderer.view)
 
+    // testing components
+
     const texture = Texture.fromImage(texture1)
     const sprite = new Sprite(texture)
     sprite.anchor.set(texture.width, texture.height)
-    sprite.position.set(20, 40)
 
-    this.stage.addChild(sprite)
+    const world = new World()
+      .addSystem(new RenderingSystem(this.stage, this.renderer), [
+        SpriteComponent.name,
+        TransformComponent.name
+      ])
+
+    const player = world.createEntity()
+    player
+      .add(new SpriteComponent(sprite))
+      .add(new TransformComponent())
+      .register()
+
+    // test
+    //this.stage.addChild(sprite)
 
     this.loop()
   }
 
   private loop() {
     requestAnimationFrame(() => this.loop())
-
-    this.renderer.render(this.stage)
+    this.world.update()
   }
 
   static main() {
