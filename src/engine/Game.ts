c829743aeeebeafea8f3b52fd7fcb538ab8cd254
ewgenius <ewgeniux@gmail.com>
2016-07-04 21:32:38 +0300

    dependencies

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -17,33 +17,6 @@ import {
 
 const texture1: string = require<string>('../../assets/texture1.jpg')
 
-class TransformComponent extends Component {
-  position: Point = new Point(0, 0)
-}
-
-class SpriteComponent extends Component {
-  sprite: Sprite
-
-  constructor(sprite: Sprite) {
-    super()
-    this.sprite = sprite
-  }
-}
-
-class RenderingSystem extends System {
-  private renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer
-  private root: Container
-
-  constructor(root: Container, renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer) {
-    super()
-    this.renderer = renderer
-    this.root = root
-  }
-
-  update() {
-    this.renderer.render(this.root)
-  }
-}
 
 export class Game {
   private renderer = autoDetectRenderer(800, 600, { backgroundColor: 0x000000 })
@@ -53,33 +26,17 @@ export class Game {
   run() {
     document.body.appendChild(this.renderer.view)
 
-    // testing components
-
     const texture = Texture.fromImage(texture1)
     const sprite = new Sprite(texture)
     sprite.anchor.set(texture.width, texture.height)
-
-    const world = new World()
-      .addSystem(new RenderingSystem(this.stage, this.renderer), [
-        SpriteComponent.name,
-        TransformComponent.name
-      ])
-
-    const player = world.createEntity()
-    player
-      .add(new SpriteComponent(sprite))
-      .add(new TransformComponent())
-      .register()
-
-    // test
-    //this.stage.addChild(sprite)
+    this.stage.addChild(sprite)
 
     this.loop()
   }
 
   private loop() {
     requestAnimationFrame(() => this.loop())
-    this.world.update()
+    this.renderer.render(this.stage)
   }
 
   static main() {
--- src/engine/ecs.ts
+++ src/engine/ecs.ts
@@ -9,7 +9,6 @@ export class System {
   }
 
   public onEntityAdded(entity) {
-
   }
 }
 
