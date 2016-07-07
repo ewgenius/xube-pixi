ac19cf742d4ecf0891944be42754dea258a8f4b5
ewgenius <ewgeniux@gmail.com>
2016-07-07 17:35:39 +0300

    upd

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -18,28 +18,12 @@ export class Game {
   run() {
     document.body.appendChild(this.renderer.view)
 
-    document.addEventListener('keydown', e => this.onKeyDown(e))
-    document.addEventListener('keypress', e => this.onKeyPress(e))
-    document.addEventListener('keyup', e => this.onKeyUp(e))
-
     const texture = Texture.fromImage(texture1)
     const sprite = new Sprite(texture)
     sprite.anchor.set(texture.width, texture.height)
     this.stage.addChild(sprite)
 
     this.loop()
-  }
-
-  onKeyDown(key) {
-    console.log(key)
-  }
-
-  onKeyUp(key) {
-    console.log(key)
-  }
-
-  onKeyPress(key) {
-    console.log(key)
   }
 
   private loop() {
